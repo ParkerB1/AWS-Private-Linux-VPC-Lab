1ea35fd87ebf472213f64404ec019efb15f984e4
Parker Blackwell <parkerblackwell7@yahoo.com>
2026-01-28 11:19:16 -0600

    update deploy and app

--- app.py
+++ app.py
@@ -6,7 +6,3 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, World!<br>Testing CI/CD"
-
-if __name__ == "__main__":
-    # Listen on all interfaces (0.0.0.0) and port 8080
-    app.run(host="0.0.0.0", port=8080)
