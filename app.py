5b50af6f04a265b12b94bde4c1e69a7d9570c1ed
Parker Blackwell <parkerblackwell7@yahoo.com>
2026-01-27 19:32:23 -0600

    update app.py

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
-from flask import Flask
+from flask import Flask, Response
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<body style='background-color:black, color:white;'>Hello, World!<br>Testing CI/CD</body>"
+    html_content = """
+    <html>
+        <body style="background-color:black; color:white; font-family:monospace; white-space:pre;">
+Hello, World!
+Testing CI/CD
+        </body>
+    </html>
+    """
+    return Response(html_content, mimtype="text/html")
 
 if __name__ == "__main__":
     # Listen on all interfaces (0.0.0.0) and port 8080
